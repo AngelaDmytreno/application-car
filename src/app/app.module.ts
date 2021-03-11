a017675e61f0ea6fb6fe313b93b571201153d4b3
AngelaDmytreno <angeladmytrenko22@gmail.com>
2021-03-11 12:15:55 +0200

    base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatIconModule} from '@angular/material/icon';
+import { MenuComponent } from './menu/menu/menu.component';
+import { MenuItemComponent } from './menu-item/menu-item/menu-item.component';
+import { HomeComponent } from './home/home/home.component';
+import { CarsComponent } from './cars/cars/cars.component';
+import { DaelersComponent } from './dealers/daelers/daelers.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    MenuComponent,
+    MenuItemComponent,
+    HomeComponent,
+    CarsComponent,
+    DaelersComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dealers/daelers/daelers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daelers',
+  templateUrl: './daelers.component.html',
+  styleUrls: ['./daelers.component.scss']
+})
+export class DaelersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
