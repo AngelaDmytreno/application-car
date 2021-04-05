88905c816f6bf48e533dcc6ad213e6007e85422f
AngelaDmytreno <angeladmytrenko22@gmail.com>
2021-04-05 21:18:41 +0300

    WIP: make form without scrol

--- src/app/shared-components/car-form/car-form.component.ts
+++ src/app/shared-components/car-form/car-form.component.ts
@@ -84,7 +84,7 @@ export class CarFormComponent implements OnInit {
     let result = '';
     let words = '0123456789';
     let max_position = words.length - 1;
-    for (let i = 0; i < 11; ++i) {
+    for (let i = 0; i < 10; ++i) {
       let position = Math.floor(Math.random() * max_position);
       result = result + words.substring(position, position + 1);
     }
